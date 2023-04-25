c31aec50f039e5803c215493206626483350b9e5
sebas1197 <eslandazuri@espe.edu.ec>
2023-04-24 21:31:39 -0500

    Paso 4

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -25,6 +25,28 @@ export const get_all_books = async (req: Request, res: Response) => {
     }
 }
 
+export const get_all_books_by_date_creation = async (req: Request, res: Response) => {
+    try {
+        const books = await Book.find().sort({date_start: req.params.date_start})
+
+        if(!books){
+            res.status(204).json({
+                msg: 'No books found',
+                books
+            })
+        }else{
+            res.status(200).json({
+                books
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: 'Error getting books',
+            error
+        })
+    }
+}
 
 export const post_book = async (req: Request, res: Response) => {
     try {
--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import { get_all_books, post_book } from '../controllers/book.controller'
+import { get_all_books, get_all_books_by_date_creation, post_book } from '../controllers/book.controller'
 
 const router = Router()
 
 router.post('/', post_book)
 router.get('/', get_all_books)
+router.get('/:date_start', get_all_books_by_date_creation)
 
 export default router
  
