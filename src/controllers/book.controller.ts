3d0457b0f5646ca0f5b75721e0b9127590f7cf34
sebas1197 <eslandazuri@espe.edu.ec>
2023-04-24 21:30:08 -0500

    Paso 3

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,13 +1,36 @@
 import { Request, Response } from 'express'
-import BookModel from '../models/book.model'
+import Book from '../models/book.model'
 
+
+export const get_all_books = async (req: Request, res: Response) => {
+    try {
+        const books = await Book.find()
+
+        if(!books){
+            res.status(204).json({
+                msg: 'No books found',
+                books
+            })
+        }else{
+            res.status(200).json({
+                books
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: 'Error getting books',
+            error
+        })
+    }
+}
 
 
 export const post_book = async (req: Request, res: Response) => {
     try {
         const {id, name, date_start, date_modification, autor, editorial} = req.body
 
-        const book = new BookModel({
+        const book = new Book({
             id,
             name,
             date_start,
--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express'
-import { post_book } from '../controllers/book.controller'
+import { get_all_books, post_book } from '../controllers/book.controller'
 
 const router = Router()
 
 router.post('/', post_book)
+router.get('/', get_all_books)
 
 export default router
+ 
